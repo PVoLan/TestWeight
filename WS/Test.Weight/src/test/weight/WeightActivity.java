b53b9bdabc9aea24d4f72ebe27fc4fcd351dcdfc
PVoLan <pvolan@mail.ru>
2012-02-28 00:28:52 +0400

    1

---
+++ WS/Test.Weight/src/test/weight/WeightActivity.java
@@ -0,0 +1,14 @@
+package test.weight;
+
+import test.weight.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WeightActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
